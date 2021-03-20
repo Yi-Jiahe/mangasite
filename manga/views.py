3325f8058a4f514fd3292bdc10f57059c6b5d0fb
Yi-Jiahe <47210363+Yi-Jiahe@users.noreply.github.com>
2021-03-20 21:40:56 +0800

    Created layout, index and series html pages and updated the index view to pass in a list of manga

--- manga/views.py
+++ manga/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from manga.models import Series, Scanlator
+
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello, world. You're at the manga index.")
+    return render(request, "manga/index.html", {
+                      'manga': Series.objects.all()
+                  })
