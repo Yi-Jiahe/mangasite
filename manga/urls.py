3e71566b9b2eb116f180d2a0d094d000fbc414d4
Yi-Jiahe <47210363+Yi-Jiahe@users.noreply.github.com>
2021-03-20 23:08:58 +0800

    Updated series links to use ids

--- manga/urls.py
+++ manga/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 
 from . import views
 
+app_name = "manga"
 urlpatterns = [
     path('', views.index, name='index'),
+    path('series/<str:id>', views.series, name='series')
 ]
--- manga/views.py
+++ manga/views.py
@@ -6,5 +6,12 @@ from manga.models import Series, Scanlator
 # Create your views here.
 def index(request):
     return render(request, "manga/index.html", {
-                      'manga': Series.objects.all()
-                  })
+        'manga': Series.objects.all()
+    })
+
+
+def series(request, id):
+    series = Series.objects.filter(id=id)[0]
+    return render(request, "manga/series.html", {
+        'series': series
+    })
